e09b30ac0184ed28e7a71104ca1a8a57746222ea
Mohit yadav <elitemohityadav@gmail.com>
2021-10-27 20:03:23 +0530

    time zone added

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-let a;
-let time;
+// let a;
+ let time;
 let date;
-setInterval(() => {
-    a= new Date();
-    date= a.toLocaleDateString();
-     time= a.getHours()+':'+a.getMinutes()+':'+a.getSeconds(); 
-     document.getElementById("Time").innerText= time;
-     document.getElementById("Date").innerHTML= date;
-}, 1000);
+let x;
+calcTime(5.5);
+// setInterval(() => {
+//     a= new Date();
+//     date= a.toLocaleDateString();
+//      time= a.getHours()+':'+a.getMinutes()+':'+a.getSeconds(); 
+//      document.getElementById("Time").innerText= time;
+//      document.getElementById("Date").innerHTML= date;
+// }, 1000);
+function calcTime(offset) {
+        
+        d = new Date();
+        x=3600000*offset;
+        setInterval(() => {
+            d = new Date();
+            utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+            nd = new Date(utc + x);
+            time= nd.toLocaleTimeString();
+            // time= nd.getHours()+':'+nd.getMinutes()+':'+nd.getSeconds();
+            date= nd.toLocaleDateString();
+            
+            document.getElementById("Time").innerHTML= time;
+            document.getElementById("Date").innerHTML= date;
+        }, 1000);
+    }
