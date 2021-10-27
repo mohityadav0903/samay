df981ca9f977120692a5b1588c931bd736a65fdb
Mohit yadav <elitemohityadav@gmail.com>
2021-10-27 16:58:17 +0530

    date added

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let a;
 let time;
+let date;
 setInterval(() => {
     a= new Date();
+    date= a.toLocaleDateString();
      time= a.getHours()+':'+a.getMinutes()+':'+a.getSeconds(); 
      document.getElementById("Time").innerText= time;
+     document.getElementById("Date").innerHTML= date;
 }, 1000);
